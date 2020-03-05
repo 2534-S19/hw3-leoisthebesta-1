0a749f96e6de76be98d01c0012bedbb242baa9f2
leoisthebesta <zhiyl17@vt.edu>
2020-03-05 16:56:22 -0500

    I love 2534

--- homework3.c
+++ homework3.c
@@ -3,14 +3,18 @@
 #include "myGPIO.h"
 #include "myTimer.h"
 
+
+typedef enum {UP,DOWN} pushbutton_state;
 int main(void)
 {
     // Count variables to control the LEDs.
     unsigned int count0 = 0;
     unsigned int count1 = 0;
 
-    // TODO: Declare the variables that main uses to interact with your state machine.
 
+    // TODO: Declare the variables that main uses to interact with your state machine.
+    uint8_t history = 0xFF;// in order to set the button history button
+    bool press = false;
 
     // Stops the Watchdog timer.
     initBoard();
@@ -36,21 +40,48 @@ int main(void)
         // TODO: If Timer0 has expired, increment count0.
         // YOU MUST WRITE timer0expired IN myTimer.c
 
-
+        if(timer0Expired())
+        {
+            if(count0 == 7)//stop at 7 and loop to 0
+           {
+                 count0 = 0;
+           }
+            else
+                 count0 += 1;
+        }
 
         // TODO: If Timer1 has expired, update the button history from the pushbutton value.
         // YOU MUST WRITE timer1expired IN myTimer.c
-
+        if(timer1Expired())
+        {
+           if(checkStatus_BoosterpackS1() == 0)
+           {
+               history = history*2 ;//  if buttom pressed, history will become 0
+           }
+           else
+           {
+               history = history*2+1;//not pressed, history will become 1
+           }
+        }
 
 
         // TODO: Call the button state machine function to check for a completed, debounced button press.
         // YOU MUST WRITE THIS FUNCTION BELOW.
 
-
+        press = fsmBoosterpackButtonS1(history);
 
         // TODO: If a completed, debounced button press has occurred, increment count1.
 
 
+        if(press == true)
+        {
+            if(count1 == 7)
+            {
+                count1 = 0;
+            }
+            else
+                count1 += 1;
+        }
 
     }
 }
@@ -64,6 +95,56 @@ void initBoard()
 // Since count is an unsigned integer, you can mask the value in some way.
 void changeLaunchpadLED2(unsigned int count)
 {
+   switch (count){
+       case 0:
+           turnOff_LaunchpadLED2Red();
+           turnOff_LaunchpadLED2Blue();
+           turnOff_LaunchpadLED2Green();
+           break;
+       case 1:
+                turnOn_LaunchpadLED2Red();//red
+                turnOff_LaunchpadLED2Blue();
+                turnOff_LaunchpadLED2Green();
+                break;
+       case 2:
+                turnOff_LaunchpadLED2Red();//green
+                turnOff_LaunchpadLED2Blue();
+                turnOn_LaunchpadLED2Green();
+                break;
+       case 3:
+                turnOff_LaunchpadLED2Red();  //Blue and green
+                turnOn_LaunchpadLED2Blue();
+                turnOn_LaunchpadLED2Green();
+                break;
+       case 4:
+                turnOff_LaunchpadLED2Red(); //blue
+                turnOn_LaunchpadLED2Blue();
+                turnOff_LaunchpadLED2Green();
+                break;
+       case 5:
+                turnOn_LaunchpadLED2Red();//blue and red
+                turnOn_LaunchpadLED2Blue();
+                turnOff_LaunchpadLED2Green();
+                break;
+       case 6:
+                turnOn_LaunchpadLED2Red(); //red and green
+                turnOff_LaunchpadLED2Blue();
+                turnOn_LaunchpadLED2Green();
+                break;
+       case 7:
+                turnOn_LaunchpadLED2Red(); //red and blue
+                turnOn_LaunchpadLED2Blue();
+                turnOff_LaunchpadLED2Green();
+                break;
+     // just in case since there are clocks
+       default:
+                turnOff_LaunchpadLED2Red();
+                turnOff_LaunchpadLED2Blue();
+                turnOff_LaunchpadLED2Green();
+                break;
+
+
+   }
 
 }
 
@@ -71,7 +152,53 @@ void changeLaunchpadLED2(unsigned int count)
 // This is essentially a copy of the previous function, using a different LED
 void changeBoosterpackLED(unsigned int count)
 {
-
+    switch (count){
+        case 0:
+            turnOff_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDBlue();
+            turnOff_BoosterpackLEDGreen();
+            break;
+        case 1:
+                 turnOn_BoosterpackLEDRed();//red
+                 turnOff_BoosterpackLEDBlue();
+                 turnOff_BoosterpackLEDGreen();
+                 break;
+        case 2:
+                 turnOff_BoosterpackLEDRed();//green
+                 turnOff_BoosterpackLEDBlue();
+                 turnOn_BoosterpackLEDGreen();
+                 break;
+        case 3:
+                 turnOff_BoosterpackLEDRed();  //Blue and green
+                 turnOn_BoosterpackLEDBlue();
+                 turnOn_BoosterpackLEDGreen();
+                 break;
+        case 4:
+                 turnOff_BoosterpackLEDRed(); //blue
+                 turnOn_BoosterpackLEDBlue();
+                 turnOff_BoosterpackLEDGreen();
+                 break;
+        case 5:
+                 turnOn_BoosterpackLEDRed();//blue and red
+                 turnOn_BoosterpackLEDBlue();
+                 turnOff_BoosterpackLEDGreen();
+                 break;
+        case 6:
+                 turnOn_BoosterpackLEDRed(); //red and green
+                 turnOff_BoosterpackLEDBlue();
+                 turnOn_BoosterpackLEDGreen();
+                 break;
+        case 7:
+                 turnOn_BoosterpackLEDRed(); //red and blue
+                 turnOn_BoosterpackLEDBlue();
+                 turnOff_BoosterpackLEDGreen();
+                 break;
+        default:
+                 turnOff_BoosterpackLEDRed();
+                 turnOff_BoosterpackLEDBlue();
+                 turnOff_BoosterpackLEDGreen();
+                 break;
+      }
 }
 
 // TODO: Create a button state machine.
@@ -79,6 +206,25 @@ void changeBoosterpackLED(unsigned int count)
 bool fsmBoosterpackButtonS1(unsigned int buttonhistory)
 {
     bool pressed = false;
+    pushbutton_state state = UP;
+    switch(state){
+        case UP:
+            if(buttonhistory == 1) // if unpressed , means that a user has finished a push.
+            {
+                state = DOWN;
+                pressed = true;
+            }
+            break;
+        case DOWN:
+            if(buttonhistory == 0)// still pushing
+            {
+                state = UP;
+                pressed = false;
+            }
+            break;
+
+                 }
+
 
 
     return pressed;
--- myTimer.c
+++ myTimer.c
@@ -12,12 +12,14 @@
 // Aliases for the Timers and the preScaler arguments are defined in myTimer.h
 void initTimer(unsigned int timer, unsigned int preScaler, unsigned int count)
 {
+    Timer32_initModule(timer,preScaler,TIMER32_32BIT,TIMER32_PERIODIC_MODE);
+
     // For the specified timer and pre-scale value, put the timer in 32-bit periodic mode.
 
-
+    Timer32_setCount(timer,count);
     // For the specified timer, pass the count value.
 
-
+    Timer32_startTimer(timer,REPEAT);
     // For the specified timer, configure the timer to repeat once it elapses.
 
 }
@@ -26,7 +28,15 @@ void initTimer(unsigned int timer, unsigned int preScaler, unsigned int count)
 // You have been given a such a function in the lecture slides.
 bool timer0Expired(void)
 {
+   static unsigned int previousSnap = MAX_VALUE;
+          unsigned int currentSnap;
+   bool value;
 
+   currentSnap = Timer32_getValue(TIMER32_0_BASE);
+   value = (currentSnap> previousSnap);
+   previousSnap = currentSnap;
+
+   return value;
 }
 
 // TODO: Write a function that indicates whether Timer1 has expired.
@@ -34,5 +44,14 @@ bool timer0Expired(void)
 // Since we only have two timers, later in the course we will create software timers that use a common time base.
 bool timer1Expired(void)
 {
+    static unsigned int previousSnap = MAX_VALUE;
+           unsigned int currentSnap;
+    bool value;
+
+    currentSnap = Timer32_getValue(TIMER32_1_BASE);
+    value = (currentSnap> previousSnap);
+    previousSnap = currentSnap;
+
+    return value;
 
 }
